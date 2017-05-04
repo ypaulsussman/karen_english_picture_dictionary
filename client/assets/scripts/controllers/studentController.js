6548ecc59c17d6de9cc8e4dcff6be6a04578e6b5
Y Paul Sussman <ypaulsussman@gmail.com>
2017-05-04 10:58:19 -0500

    continues S3 image transition

--- client/assets/scripts/controllers/studentController.js
+++ client/assets/scripts/controllers/studentController.js
@@ -1,7 +1,6 @@
 myApp.controller('StudentController', ['$scope', '$http', '$location', 'ItemService', 'UserService', function($scope, $http, $location, ItemService, UserService) {
   UserService.validateStudentRole();
   $scope.userName = UserService.user;
-  console.log("here's the data we have at the StudentController: ", $scope.userName);
   $scope.logout = UserService.logout;
 
   $scope.themes = ItemService.themes;
